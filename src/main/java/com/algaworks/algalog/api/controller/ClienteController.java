f35db4ac7c43b296e1ad95b27a4adcdea67f3c06
Jonathan Santos <jonathan.santos@clamed.com.br>
2022-07-21 14:31:20 -0300

    Alterações no projeto

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,11 +3,12 @@ package com.algaworks.algalog.api.controller;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.rsocket.context.RSocketPortInfoApplicationContextInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -21,13 +22,44 @@ public class ClienteController {
 
     @GetMapping
     public List<Cliente> listar() {
-        return clienteRepository.findByNomeContaining("a");
+        return clienteRepository.findAll();
 
     }
 
     @GetMapping("/{id}")
-    public Cliente buscar(@PathVariable UUID id) {
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-        return cliente.orElse(null);
+    public ResponseEntity<Cliente> buscar(@PathVariable UUID id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente>  atualizar(@PathVariable UUID clienteId, @RequestBody Cliente cliente) {
+        if(!clienteRepository.existsById(clienteId)) {
+            ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> remover(@PathVariable UUID clienteId) {
+        if (!clienteRepository.existsById(clienteId)) {
+           return ResponseEntity.notFound().build();
+        }
+
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
     }
 }
